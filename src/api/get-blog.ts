5b8f909a9d3ff68927d22a94d8d24f74c368147a
Arkungking <aryagiffari0@gmail.com>
2025-11-05 10:31:36 +0700

    feat: add join on get blog by slug

--- src/api/get-blog.ts
+++ src/api/get-blog.ts
@@ -1,8 +1,10 @@
+import { Blog } from "@/types/blog";
 import { cache } from "react";
 
-const url = process.env.NEXT_PUBLIC_BASE_URL_API!;
+const url = process.env.NEXT_PUBLIC_BASE_URL_API2!;
 
-export const getBlog = cache(async (objectId: string) => {
-  const response = await fetch(url + "/api/data/Blogs/" + objectId);
-  return await response.json();
+export const getBlog = cache(async (slug: string) => {
+  const response = await fetch(url + "/blogs/" + slug);
+  const blog: Blog = await response.json();
+  return blog;
 });
